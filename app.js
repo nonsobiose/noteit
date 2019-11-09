84088d0f8fc0da92c626db425d8e0b8661ce17a7
Emmanuel Biose <slimbion@gmail.com>
2019-11-09 22:33:10 +0000

    changed to hbs

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
+var hbs = require('express-handlebars');
 
+app.set('view engine', 'hbs')
 app.get('/redirect', (req, res) => {
-    res.render('./redirect.html');
+    res.render('./redirect');
 });
 app.get('/startnote', (req, res) => res.send('This starts a note session'));
 app.get('/endnote', (req, res) => res.send('This ends a note session'));
